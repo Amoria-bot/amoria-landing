5b8219d7bb1fc1ce99c55d271017b8cc5dcf3ec6
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2024-12-12 20:16:56 +0200

    add title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import HomeOne from "@/component/homes/home-one";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-  title: "IKO - ICO & Crypto Landing Page React Next js Template",
+    title: "Amoria: Candid Conversations Without Boundaries",
+    description: "Discover Amoria — the platform for intimate and honest dialogues. Choose unique AI-generated companions, explore your desires, and dive into conversations where there are no limits. Completely confidential, only for users 18 and older."
 };
 const index = () => {
-  return (
-    <Wrapper>
-      <HomeOne />
-    </Wrapper>
-  )
+    return (
+        <Wrapper>
+            <HomeOne/>
+        </Wrapper>
+    )
 }
 
 export default index
